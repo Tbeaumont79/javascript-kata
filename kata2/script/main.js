3d3946c5da798e4d618f3f1f924f76587de630ef
tbeaumont79 <tbeaumont79@icloud.com>
2025-05-23 16:48:54 +0200

    feat: now display the content post by default even if there is no filter

--- kata2/script/main.js
+++ kata2/script/main.js
@@ -1,11 +1,6 @@
 const API_URL = "https://dummyjson.com/posts";
 const getInput = document.getElementById("search-id");
-getInput.addEventListener("input", async (e) => {
-	const currentValue = e.target.value;
-	const postDatas = await getPosts();
-	const filteredArray = arrayFilter(postDatas, currentValue);
-	e.target.value !== "" ? displayCards(filteredArray) : displayCards(postDatas);
-});
+const mainElement = document.getElementsByTagName("main");
 
 function arrayFilter(array, seachTerm) {
 	return array.filter(
@@ -31,17 +26,15 @@ async function getPosts() {
 	}
 }
 
-async function displayCards(posts) {
-	const mainElement = document.getElementsByTagName("main");
-	await posts.map((items) => {
-		console.log(items);
+async function displayCards(parrentElem, posts) {
+	parrentElem[0].innerHTML = "";
+	posts.map((items) => {
 		postCard(mainElement, items.title, items.body, items.reactions, items.tags);
 	});
 }
 
 async function postCard(mainElement, title, body, reactions, tags) {
 	const section = document.createElement("section");
-	section.innerHTML = "";
 	mainElement[0].appendChild(section);
 	section.innerHTML = `
 	<div class="card bg-base-100 w-96 shadow-sm">
@@ -60,3 +53,16 @@ async function postCard(mainElement, title, body, reactions, tags) {
 		</div>
 	</div>`;
 }
+
+getInput.addEventListener("input", async (e) => {
+	const currentValue = e.target.value;
+	const postDatas = await getPosts();
+	const filteredArray = arrayFilter(postDatas, currentValue);
+	displayCards(mainElement, filteredArray);
+});
+
+async function init() {
+	const posts = await getPosts();
+	displayCards(mainElement, posts);
+}
+init();
