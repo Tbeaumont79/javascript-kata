fd7e83c695a8ed37dd5288d71fc0ea0fbc2d8efa
tbeaumont79 <tbeaumont79@icloud.com>
2025-10-23 20:27:40 +0200

    feat: adding a login form

--- kata4/front/src/App.vue
+++ kata4/front/src/App.vue
@@ -1,11 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Login from './pages/LoginPage.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>This is the front End</h1>
+  <Login />
 </template>
 
 <style scoped></style>
---
+++ kata4/front/src/pages/LoginPage.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const username = ref('')
+const password = ref('')
+async function auth(event: Event) {
+  event.preventDefault()
+  console.log('username', username.value)
+  console.log('password', password.value)
+}
+</script>
+
+<template>
+  <h1>Login</h1>
+  <form @submit="auth">
+    <label for="username">Username: </label>
+    <input id="username" placeholder="enter your username " v-model="username" />
+    <label for="password">Password: </label>
+    <input id="password" type="password" placeholder="************" v-model="password" />
+    <button type="submit">Login</button>
+  </form>
+</template>
